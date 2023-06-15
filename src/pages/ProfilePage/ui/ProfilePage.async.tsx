af5e24b49fbe71b5f412c15e59401d93d1acc56b
Zebych <81613417+Zebych@users.noreply.github.com>
2023-06-15 15:00:13 +0300

    production

--- src/pages/ProfilePage/ui/ProfilePage.async.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-// export const MainPageAsync = lazy(() => import('./MainPage'))
-export const ProfilePageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./ProfilePage')), 1500);
-}));
+export const ProfilePageAsync = lazy(() => import('./ProfilePage'));
