9f5002030651163f11b55340dc9bd68bcc2555eb
Zebych <81613417+Zebych@users.noreply.github.com>
2023-06-15 15:18:08 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
-// const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : 'http://production.ru';
-
 export const $api = axios.create({
-    // baseURL: baseUrl,
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+
+    return config;
 });
