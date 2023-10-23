662a8d6c1295109da4e8a3151c031bbb1e4e0a76
Zebych <81613417+Zebych@users.noreply.github.com>
2023-10-23 16:50:02 +0300

    add toggle features for components

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,7 +56,7 @@ module.exports = {
                     'justify',
                     'align',
                     'direction',
-                    'gap',
+                    'feature',
                 ],
             },
         ],
